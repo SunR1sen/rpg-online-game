10d002fce8faae69b5b344cc70a98b6b072da25f
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-07-03 21:39:38 +0300

    feat: add character on canvas and moving down

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import './index.scss';
+import CharacterWalk from './assets/main-character-walk.png';
 
-console.log('HELLO WORLD!');
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+const img = document.createElement('img');
+const spriteWidth = 48;
+const spriteHeight = 48;
+const shots = 3;
+let cycle = 0;
+let bottomPressed = false;
+let pY = 0;
+
+const keyDownHandler = (e) => {
+    console.log(e);
+    if (e.key === "Down" || e.key === "ArrowDown") {
+        bottomPressed = true;
+    }
+};
+
+const keyUpHandler = (e) => {
+    console.log(e);
+    if (e.key === "Down" || e.key === "ArrowDown") {
+        bottomPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+
+img.src = CharacterWalk;
+
+img.addEventListener("load", () => {
+    setInterval(() => {
+        if (bottomPressed) {
+            pY += 10;
+            cycle = (cycle + 1) % shots;
+        }
+        ctx.clearRect(0, 0, 600, 600);
+        ctx.drawImage(img, cycle * spriteWidth, 0, spriteWidth, spriteHeight, 0, pY, 100, 100);
+    }, 120)
+})
+
