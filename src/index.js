785f4c6d5e23e40f24a46314691143e335e1a06d
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-07-03 22:40:52 +0300

    feat: add moving to all 4 directions

--- src/index.js
+++ src/index.js
@@ -9,37 +9,74 @@ const spriteWidth = 48;
 const spriteHeight = 48;
 const shots = 3;
 let cycle = 0;
-let bottomPressed = false;
+const characterMove = {
+  bottom: false,
+  up: false,
+  left: false,
+  right: false,
+};
 let pY = 0;
+let pX = 0;
 
 const keyDownHandler = (e) => {
-    console.log(e);
     if (e.key === "Down" || e.key === "ArrowDown") {
-        bottomPressed = true;
+        characterMove.bottom = true;
+    }
+    if (e.key === "Up" || e.key === "ArrowUp") {
+        characterMove.up = true;
+    }
+    if (e.key === "Left" || e.key === "ArrowLeft") {
+        characterMove.left = true;
+    }
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        characterMove.right = true;
     }
 };
 
 const keyUpHandler = (e) => {
-    console.log(e);
     if (e.key === "Down" || e.key === "ArrowDown") {
-        bottomPressed = false;
+        characterMove.bottom = false;
     }
-}
+    if (e.key === "Up" || e.key === "ArrowUp") {
+        characterMove.up = false;
+    }
+    if (e.key === "Left" || e.key === "ArrowLeft") {
+        characterMove.left = false;
+    }
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        characterMove.right = false;
+    }
+};
 
-document.addEventListener("keydown", keyDownHandler);
-document.addEventListener("keyup", keyUpHandler);
-
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
 
 img.src = CharacterWalk;
+let spritePosY = 0;
 
-img.addEventListener("load", () => {
-    setInterval(() => {
-        if (bottomPressed) {
-            pY += 10;
-            cycle = (cycle + 1) % shots;
-        }
-        ctx.clearRect(0, 0, 600, 600);
-        ctx.drawImage(img, cycle * spriteWidth, 0, spriteWidth, spriteHeight, 0, pY, 100, 100);
-    }, 120)
-})
-
+img.addEventListener('load', () => {
+  setInterval(() => {
+    if (characterMove.bottom) {
+      pY += 10;
+      spritePosY = 0;
+      cycle = (cycle + 1) % shots;
+    }
+    if (characterMove.up) {
+      pY -= 10;
+      spritePosY = spriteHeight * 3;
+      cycle = (cycle + 1) % shots;
+    }
+    if (characterMove.left) {
+      pX -= 10;
+      spritePosY = spriteHeight;
+      cycle = (cycle + 1) % shots;
+    }
+    if (characterMove.right) {
+      pX += 10;
+      spritePosY = spriteHeight * 2;
+      cycle = (cycle + 1) % shots;
+    }
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.drawImage(img, cycle * spriteWidth, spritePosY, spriteWidth, spriteHeight, pX, pY, 48, 48);
+  }, 120);
+});
