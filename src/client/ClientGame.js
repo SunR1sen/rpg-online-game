e8af386776813a35337386ef07fdb35d2ea53188
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-07-16 09:47:50 +0300

    feat: render world cell by cell

--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -2,11 +2,13 @@ import ClientEngine from './ClientEngine';
 import sprites from '../config/sprites';
 import ClientWorld from './ClientWorld';
 import levelCfg from '../config/world.json';
+import gameObjects from '../config/gameObjects.json';
 
 class ClientGame {
   constructor(cfg) {
     Object.assign(this, {
       cfg,
+      gameObjects,
     });
 
     this.engine = this.createEngine();
@@ -24,8 +26,9 @@ class ClientGame {
 
   initEngine() {
     this.engine.loadSprites(sprites).then(() => {
+      this.world.init();
       this.engine.on('render', (_, time) => {
-        this.world.init();
+        this.world.render(time);
       });
       this.engine.start();
     });
